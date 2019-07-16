320baa11ff45515b7b220bd1c65dcb11e48d6194
HHawking <hhawking@aliyun.com>
2019-07-16 16:28:53 +0800

    1

---
+++ src/main/java/com/hhawking/demo/fastdfs/DemoFastdfsApplication.java
@@ -0,0 +1,13 @@
+package com.hhawking.demo.fastdfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFastdfsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoFastdfsApplication.class, args);
+    }
+
+}
